cbd4fccae365cb904da1554d18f45702619d5b99
jerdak_win7 <jerdak@gmail.com>
2013-11-20 15:08:54 -0500

    -  Refactor slightly to allow for linux support

--- EventTimer/EventTimer.h
+++ EventTimer/EventTimer.h
@@ -1,140 +1,204 @@
 #ifndef __EVENT_TIMER_H__
 #define __EVENT_TIMER_H__
 
-#include <windows.h>
-  
-namespace Utilities {
-	namespace Common{
-		struct ProfTimer {
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <utility>
+
+#if (defined(_WIN32) || defined(_WIN64)) && !defined(__MINGW32__)
+	#include <windows.h>
+#elif defined(__GNUC__)
+	#include <sys/time.h> 
+#endif  
+
+namespace utilities {
+	/**	High resolution timer
+		
+		*Note:  Old documentation on MSDN [1] suggests using SetThreadAffinityMask
+		as bugs in BIOS could result in inconsistent timings on multicore systems.
+		Anecdotally we've seen this behaviour in the lab resulting in several negative
+		deltas across a variety of hardware. 
+
+		To mimic SetThreadAffinity in a *Nix environment I use clock_gettime() instead
+		of just gettimeofday().  gettimeofday() is subject to modifications of the
+		*Nix "wall-clock".  Something like NTP can alter the clock giving the user
+		negative time deltas.
+		
+		Links
+		[1] - http://msdn.microsoft.com/en-us/library/windows/desktop/ee417693(v=vs.85).aspx
+	*/
+	class ProfTimer {
+	public:
+		ProfTimer(){}
+		
+		#if (defined(_WIN32) || defined(_WIN64)) && !defined(__MINGW32__)
+			// Start timer
 			void Start(void) {
 				DWORD_PTR threadAffMask = SetThreadAffinityMask(GetCurrentThread(),1);
 
-				QueryPerformanceFrequency(&ticksPerSecond);
-				QueryPerformanceCounter(&tick);
-				
-				SetThreadAffinityMask(GetCurrentThread(),threadAffMask);
+				QueryPerformanceFrequency(&ticks_per_second_);
+				QueryPerformanceCounter(&tick_);
+				SetThreadAffinityMask(GetCurrentThread(),threadAffMask);     //make sure to revert affinity
 			};
+			
+			// Stop timer
 			void Stop(void) {
 				DWORD_PTR threadAffMask = SetThreadAffinityMask(GetCurrentThread(),1);
-				QueryPerformanceCounter(&tock);
-				SetThreadAffinityMask(GetCurrentThread(),threadAffMask);
+				
+				QueryPerformanceCounter(&tock_);
+				SetThreadAffinityMask(GetCurrentThread(),threadAffMask);	//make sure to revert affinity
 			};
-			double GetDurationInSecs(void)
+			
+			// Get duration between last call to Start() and Stop(), respectively.
+			virtual double GetDurationInSecs(void)
 			{
-				double duration = (double)(tock.QuadPart-tick.QuadPart)/(double)ticksPerSecond.QuadPart;
+				double duration = (double)(tock_.QuadPart-tick_.QuadPart)/(double)ticks_per_second_.QuadPart;
 				return duration;
 			}
-  			LARGE_INTEGER ticksPerSecond;
-			LARGE_INTEGER tick;   // A point in time
-			LARGE_INTEGER tock;
-			LARGE_INTEGER time;   // For converting tick into real time
-		};
-
-		class EventTimer : public ProfTimer {
-		public:
-			EventTimer(){
-
-				sprintf(name,"Default");
-				Init();
-			};
-			EventTimer(char *pName){
-				sprintf(name,"%s",pName);
-				Init();
-			};
-			~EventTimer(){
-			
-			}
-			void Init(){
-				_nCalled = 0;
-				_dDuration = 0;
-				_dBefore = 0;
-				_dAfter = 0;
-				_isTiming = false;
-				_isPaused = false;
-				_isEnabled = true;
-
-				Start();
-			}
-			void Disable(){ _isEnabled = false; }
-			void Enable() { _isEnabled = true;  }
-			void BeforeEvent(){
-				if(!_isEnabled)return;
-				if(_isTiming)printf("Warning:  EventTimer[%s] before event called twice without a call to After Event.\n",name);
-				
-				if(_isPaused){
-					printf("Warning:  BeforeEvent[%s] called before UnPause\n",name);
-					UnPause();
-				}
-				_isTiming = true;
-				Stop();
-				_dBefore = GetDurationInSecs();
-				_dPauseDuration = 0;
-			}
-			void AfterEvent(){
-				if(!_isEnabled)return;
-				if(!_isTiming){
-					printf("EventTimer[%s] needs to call BeforeEvent() first.  Ignoring timing\n",name);
-					return;
-				}
-				if(_isPaused){
-					printf("Warning:  AfterEvent[%s] called before unpause.\n",name);
-					UnPause();
-				}
-				_isTiming = false;
-				Stop();
-				_dAfter = GetDurationInSecs();
-				_nCalled++;
-				//printf("%f %f\n",_dAfter,_dBefore);
-				_dDuration+=((_dAfter-_dBefore)- _dPauseDuration);
-			}
-			double GetTimeSinceLastEvent() const { return _dAfter - _dBefore; }
-			double GetDuration() const { return _dDuration; }
-			void Pause(){
-				if(!_isEnabled)return;
-				if(_isPaused)printf("Warning:  EventTimer[%s] Pause() called twice in a row\n",name);
-
-				Stop();
-				_dPauseBefore = GetDurationInSecs();
-				_isPaused = true;
-			}
-			void UnPause(){
-				if(!_isEnabled)return;
-				Stop();
-				_dPauseAfter = GetDurationInSecs();
-
-				_dPauseDuration+=(_dPauseAfter - _dPauseBefore);
-				_isPaused = false;
-			}
-			void Display(){
-				if(!_isEnabled)return;
-				printf(" -- EventTimer: %s ----\n",name);
-				printf("   - Number of Calls: %d\n",_nCalled);
-				printf("   - Total Time: %f\n",_dDuration);
-				printf("   - Timer per event: %f\n",_dDuration / _nCalled);
-				printf(" ----------------------\n\n");
-			}
-			void SetName(char *aName){
-				sprintf(name,"%s",aName);
-			}
-			bool isTiming() const { return _isTiming;}
-			int GetNumCalled() const { return _nCalled;}
-			double GetTotalTime() const { return _dDuration;}
-			double GetTimePerEvent() const { return _dDuration / _nCalled; }
-			
 		private:
-			char name[256];
-			bool _isTiming;
-			bool _isPaused;
-			bool _isEnabled;
-			int _nCalled;
-
-			double _dPauseBefore, _dPauseAfter;
-			double _dPauseDuration;
-
-			double _dBefore,_dAfter;
-			double _dDuration;
-		};
+			LARGE_INTEGER ticks_per_second_;
+			LARGE_INTEGER tick_;   // A point in time
+			LARGE_INTEGER tock_;
+		#elif defined(__GNUC__)
+		
+			// Start timer
+			void Start(){
+				clock_gettime(CLOCK_MONOTONIC,&tick_);
+			}
+			
+			// Stop timer
+			void Stop() {
+				clock_gettime(CLOCK_MONOTONIC,&tock_);
+			}
+			
+			// Get duration between last call to Start() and Stop(), respectively.
+			virtual double GetDuration(){
+				double ret = (tock_.tv_sec - tick_.tv_sec) * 1000;	//seconds to milliseconds
+				ret += (tock_.tv_nsec - tick_.tv_nsec) * 1.0e-6;    //nanoseconds to milliseconds
+				return ret;
+			}
+		private:
+			timespec tick_;
+			timespec tock_;
+		#else 
+			// TODO:  Test other implementations
+			#pragma message("Unsupported compiler")
+		#endif
+		
+		public:
+			virtual double GetDurationSeconds(){
+				return GetDuration()*1000.0;
+			}
 	};
-};
+	/**	Event timer class
+	
+		Time multiple events with allowances for pausing.
+	*/
+	class EventTimer : public ProfTimer {
+		enum EVSTATUS { EVS_STARTED=0x1,EVS_PAUSED=0x4, EVS_ENABLED=0x8};
+	public:
+		EventTimer():
+			name_("noname")
+		{Reset();}
+		
+		EventTimer(const std::string &name):
+			name_(name)
+		{Reset();}
+		
+		// Reset timer, should always be called before method 'Before()'
+		void Reset(){
+			status_ = EVS_ENABLED;
+			pause_duration_ = 0.0;
+			event_duration_ = 0.0;
+			num_calls_ = 0;
+			
+			event_durations_ = std::make_pair (0,0);
+			pause_durations_ = std::make_pair (0,0);
+			Start();
+		}
+	
+		bool Enabled(){return status_&EVS_ENABLED;}
+		bool Started() {return status_&EVS_STARTED;}
+		bool Paused() {return status_&EVS_PAUSED;}
+		
+		// Call once before event to be timed
+		void Before(){
+			if(!Enabled())return;
+			if(Started()) {
+				printf("warning, event restarted without being stopped.");
+			}
+			if(Paused()){
+				printf("Warning:  BeforeEvent[%s] called before UnPause\n",name_.c_str());
+				Unpause();	
+			}
+			Stop();
+			event_durations_.first = ProfTimer::GetDuration();
+			
+			status_ = EVSTATUS(status_ | EVS_STARTED);
+		}
+		
+		// Call any number of times after 'Before()' to time multiple events.
+		void After(){
+			if(!Enabled())return;
+			if(!Started()) {
+				printf("error, event not started");
+				return;
+			}
+			if(Paused()){
+				printf("Warning:  AfterEvent[%s] called before UnPause\n",name_.c_str());
+				Unpause();	
+			}
+			num_calls_ += 1;
+			
+			Stop();
+			event_durations_.second = ProfTimer::GetDuration();
+			event_duration_ = (event_durations_.second - event_durations_.first) - pause_duration_;
+			//status_ = EVSTATUS(status_ & ~EVS_STARTED);
+		}
+		
+		// Unpause timer
+		void Unpause(){
+			status_ = EVSTATUS(status_ & ~EVS_PAUSED);
+			Stop();
+			
+			pause_durations_.second = ProfTimer::GetDuration();
+			pause_duration_ += (pause_durations_.second - pause_durations_.first);
+		}
+		
+		// Pause timer, event duration is ignored while paused.
+		void Pause(){
+			status_ = EVSTATUS(status_ | EVS_PAUSED);
+			Stop();
+			
+			pause_durations_.first = ProfTimer::GetDuration();
+		}
+		double GetDuration(){
+			return event_duration_;
+		}
+		
+		void Dump(){
+			printf("Eventtimer[%s]\n",name_.c_str());
+			printf("  - IsEnabled: %d\n",Enabled());
+			printf("  - IsStarted: %d\n",Started());
+			printf("  - IsPaused: %d\n",Paused());
+			printf("  - Number of calls: %d\n",num_calls_);
+			printf("  - Total Time: %f\n",event_duration_);
+			printf("  - Time per event: %f\n",event_duration_/num_calls_);
+			
+		}
+		
+	private:
+		int num_calls_;
+		std::pair<double,double> event_durations_;
+		std::pair<double,double> pause_durations_;
+		
+		double event_duration_;
+		double pause_duration_;
+		
+		std::string name_;
+		EVSTATUS status_;
+	};
+}
 
 #endif //__EVENT_TIMER_H__
---
+++ Usage.cpp
@@ -0,0 +1,38 @@
+#include "EventTimer/EventTimer.h"
+
+#include <chrono>
+#include <thread>
+
+int main(){
+
+	{	// Simple timer, 1.65 seconds
+		utilities::ProfTimer p;
+		p.Start();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1565));
+		p.Stop();	//total duration = 1.65 seconds
+		
+		printf("Duration: %f ms\n",p.GetDuration());
+	}
+	
+	{ 	// Event timer, 1.25 seconds, 0.3 second pause (not included in total duration)
+		utilities::EventTimer e("timer1");
+		e.Before();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1250));
+		e.Pause();
+		std::this_thread::sleep_for(std::chrono::milliseconds(300));
+		e.Unpause();
+		e.After();
+		e.Dump();	//total duration = 1.25 seconds
+	}
+	
+	{	// Event timer, 1.0 seconds, 500 iterations (2 ms per iteration)
+		utilities::EventTimer e2("multitimer");
+		e2.Before();
+		for(int i = 0; i < 500; i++){
+		std::this_thread::sleep_for(std::chrono::milliseconds(2));
+			e2.After();
+		}
+		e2.Dump();	//total duration = 1 second
+	}
+	return 0;
+}
